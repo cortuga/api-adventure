1e1178bdfe74905a34f13c6521250b46721858a0
Corey Grandmaison <cgsustainability@gmail.com>
2019-09-04 18:35:02 -0400

    got search working. now need to process data

--- src/components/numSearchPage.js
+++ src/components/numSearchPage.js
@@ -1,33 +1,46 @@
-import React from "react"
-import react, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 const NumSearchPage = () => {
   // creates state for getters and setters
   const [quote, setQuote] = useState([0])
+  const [searchTerm, setSearchTerm] = useState([])
 
-  useEffect(() => {
-    //update the document using the dom
-    //calls a function every time the component finishes rendering
-  })
+  // useEffect(() => {
+  //   //update the document using the dom
+  //   //calls a function every time the component finishes rendering
+  //   fetchData()
+  // })
 
-  useEffect(async () => {
+  const fetchData = async () => {
     const response = await axios.get(
-      `https://programming-quotes-api.herokuapp.com/quotes`
+      `http://numbersapi.com/${searchTerm}`
+
       //  const quote = (response.data[0].en)
       //must index data(mean must look through arrays to generate )
     )
-    console.log(response.data[0].en) //correct path
+
+    // console.log(response.data[0].en) //correct path
     console.log(response)
-    setQuote(response.data[0].en)
-  })
+    // setQuote(response)
+  }
 
   // const randomQuote = () => {}
 
   return (
     <div>
-      <h1>Ontological Quote of the Day</h1>
+      <header>
+        <h1>Ontological Quote of the Day</h1>
+      </header>
       <section>
+        <input
+          id='search-text'
+          placeholder='Enter your Number'
+          type='text'
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        ></input>
+        <button onClick={fetchData}>Enter</button>
         <h2>{quote}</h2>
         <button>Random Quote!</button>
       </section>
